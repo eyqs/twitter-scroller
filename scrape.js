5296c654b049a23ef74520d4ef8c10e6aaa5dce9
Eugene Y. Q. Shen <eugene@eyqs.ca>
2017-08-27 13:52:18 -0700

    Update scroller using MutationObserver
    Navigate to the right URL "search?f=tweets" instead of "search?l=",
    catch the rejection whenever "div.stream-container" does not exist,
    properly pluralize the number of tweets found, properly slice URLs,
    print out the output file's name and kill Chromium after finishing,
    and use MutationObserver to set a flag to true whenever new tweets
    are loaded, rather than using querySelectorAll to count the number.

--- scrape.js
+++ scrape.js
@@ -12,9 +12,8 @@ const CDP = require("chrome-remote-interface");
 const fs = require("fs");
 const MONTHS_PER_YEAR = 12;
 const DAYS_PER_MONTH = 31;
-const TWEET_INCREMENT = 10;
-const SCROLL_DELAY = 300;
-const DAY_DELAY = 5000;
+const MAX_SCROLL_WAIT = 3000;
+const MAX_PAGE_WAIT = 10000;
 
 const user = process.argv[2];
 const start_year = parseInt(process.argv[3]);
@@ -27,12 +26,12 @@ const tweets = {};
 
 async function crawlTweets(Page, y, m, d) {
   const url =
-`https://twitter.com/search?l=&q=from%3A${user}%20since%3A\
+`https://twitter.com/search?f=tweets&q=from%3A${user}%20since%3A\
 ${y}-${m}-${d}%20until%3A${y}-${m}-${d + 1}&src=typd`;
   console.log(`Crawling @${user}'s tweets on ${y}-${m}-${d}...`);
   await Page.navigate({url});
   await Page.loadEventFired();
-  await new Promise(resolve => setTimeout(resolve, DAY_DELAY));
+  await new Promise(resolve => setTimeout(resolve, MAX_PAGE_WAIT));
 }
 
 (async () => {
@@ -45,32 +44,47 @@ ${y}-${m}-${d}%20until%3A${y}-${m}-${d + 1}&src=typd`;
   Page.loadEventFired(async () => {
     const result = await Runtime.evaluate({awaitPromise: true, expression: `
       (async () => {
-        let num_tweets = 0;
-        do {
-          num_tweets += ${TWEET_INCREMENT};
+        let new_tweet = true;
+        let callback = () => {};
+        const observer = new MutationObserver(function () {
+          new_tweet = true;
+          callback();
+        });
+        observer.observe(document.querySelector("div.stream-container"),
+            {attributes: true, attributeFilter: ["data-min-position"]});
+        while (new_tweet) {
+          new_tweet = false;
           window.scrollTo(0, document.body.scrollHeight);
-          await new Promise(resolve => setTimeout(resolve, ${SCROLL_DELAY}));
-        } while (document.querySelectorAll("li.js-stream-item").length
-            >= num_tweets);
+          await new Promise(function (resolve) {
+            callback = resolve;
+            setTimeout(resolve, ${MAX_SCROLL_WAIT});
+          });
+        }
       })().then(resolve => JSON.stringify({
         ids: [...document.querySelectorAll("li.js-stream-item")]
             .map(id => id.getAttribute("data-item-id")).slice(1),
         tweets: [...document.querySelectorAll("p.js-tweet-text")]
             .map(tweet => tweet.textContent),
-      }));`
+      }), reject => null);`
     });
-    const value = JSON.parse(result.result.value);
-    console.log(`...${value.ids.length} tweets found.`);
-    for (let i = 0; i < value.ids.length; i++) {
-      if (value.tweets[i].includes("pic.twitter.com")) {
-        tweets[value.ids[i]] = value.tweets[i].slice(
-            0, value.tweets[i].lastIndexOf("pic.twitter.com"));
-      } else if (value.tweets[i].includes("…")) {
-        tweets[value.ids[i]] = value.tweets[i].slice(
-            0, value.tweets[i].lastIndexOf("http"));
-      } else {
+
+    if (result.result.value) {
+      const value = JSON.parse(result.result.value);
+      const len = value.ids.length;
+      console.log(`...${len} tweet${len == 1 ? "" : "s"} found.`);
+      for (let i = 0; i < len; i++) {
+        if (value.tweets[i].includes("pic.twitter.com")) {
+          value.tweets[i] = value.tweets[i].slice(
+              0, value.tweets[i].lastIndexOf("pic.twitter.com"));
+        }
+        if (value.tweets[i].includes("…")) {
+          value.tweets[i] = value.tweets[i].slice(
+              0, value.tweets[i].lastIndexOf("http"));
+        }
         tweets[value.ids[i]] = value.tweets[i];
       }
+    } else {
+      console.log("...0 tweets found.");
     }
   });
 
@@ -101,7 +115,12 @@ ${y}-${m}-${d}%20until%3A${y}-${m}-${d + 1}&src=typd`;
     await crawlTweets(Page, year, month, day);
     ++day;
   }
-  fs.writeFile(`${user}--${start_year}-${start_month}-${start_day}--\
-${end_year}-${end_month}-${end_day}--tweets.json`, JSON.stringify(tweets),
+
+  const file_name = `${user}--${start_year}-${start_month}-${start_day}--\
+${end_year}-${end_month}-${end_day}--tweets.json`;
+  console.log(`Total number of tweets saved in "${file_name}": \
+${Object.keys(tweets).length}.`);
+  fs.writeFile(file_name, JSON.stringify(tweets),
       error => { console.log(error); process.exit(); })
+  chrome.kill();
 })();
